df6bc54a549215e1c2e98d5dca2d58ae6ff200f3
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-26 05:56:06 -0600

    Initial commit

---
+++ src/teenSum.java
@@ -0,0 +1,10 @@
+public class teenSum {
+  public int teenSum(int a, int b) {
+    if ((a > 12 && a < 20) || (b > 12 && b < 20)) {
+      return 19;
+    } else {
+      return a + b;
+    }
+  }
+
+}
